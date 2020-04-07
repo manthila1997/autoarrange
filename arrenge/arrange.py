225963e0aa32a06fedc873e6a5ad3d7d5f4b8cd1
Manthila <manthilab@gmail.com>
2020-04-07 13:14:17 +0530

    final product

--- arrenge/arrange.py
+++ arrenge/arrange.py
@@ -27,7 +27,7 @@ def arrange():
     for file in files_list:
         fextension = file.split(sep=".")
         try:
-            os.rename(file, fextension[1]+"_files/"+file)
+            os.rename(file, fextension[-1]+"_files/"+file)
         except (OSError, IndexError):
             continue
 
