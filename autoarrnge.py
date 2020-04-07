493013447f6677f89515cd329812ca885f1e77b5
Manthila <manthilab@gmail.com>
2020-04-07 12:21:21 +0530

    found this

---
+++ autoarrnge.py
@@ -0,0 +1,36 @@
+import os
+import glob
+
+files_list = glob.glob("*")
+
+extension_set = set()
+
+#adding each type of extension to the set
+for file in files_list:
+    extension = file.split(sep=".")
+    try:
+        extension_set.add(extension[1])
+    except IndexError:
+        continue
+
+#print(extension_set)
+#Function to create directory for each type of extension
+def createDirs():
+    for dir in extension_set:
+        try:
+            os.makedirs(dir+"_files")
+        except FileExistsError:
+            continue
+
+#Function to move files to respective folders
+def arrange():
+    for file in files_list:
+        fextension = file.split(sep=".")
+        try:
+            os.rename(file, fextension[1]+"_files/"+file)
+        except (OSError, IndexError):
+            continue
+
+#Calling the functions in order
+createDirs()
+arrange()
